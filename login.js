9efa79e9713cb28cb84dad650bda3e23750f42c5
sanyam-katoch10 <142176459+sanyam-katoch10@users.noreply.github.com>
2023-11-29 02:01:34 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,35 +1,31 @@
+function checkInputs(formId, buttonId, destination) {
+  const form = document.getElementById(formId);
+  const button = document.getElementById(buttonId);
 
-  function checkInputs(formId, buttonId) {
-    const form = document.getElementById(formId);
-    const button = document.getElementById(buttonId);
+  const inputs = form.querySelectorAll('input[required]');
 
-    const inputs = form.querySelectorAll('input[required]');
-
-    let allFilled = true;
-    inputs.forEach(input => {
-      if (input.value.trim() === '') {
-        allFilled = false;
-      }
-    });
-
-    if (allFilled) {
-      button.removeAttribute('disabled');
-      button.addEventListener('click', function() {
-        window.location.href="index.html";
-      });
-    } else {
-      button.setAttribute('disabled', 'true');
-      button.removeEventListener('click', function() {
-        window.location.href="index.html";
-      });
+  let allFilled = true;
+  inputs.forEach(input => {
+    if (input.value.trim() === '') {
+      allFilled = false;
     }
+  });
+
+  if (allFilled) {
+    button.removeAttribute('disabled');
+    button.onclick = function() {
+    window.location.href = "index.html";
+    };
+  } else {
+    button.setAttribute('disabled', 'true');
+    button.onclick = null;
   }
+}
 
-  document.getElementById('signupForm').addEventListener('input', function() {
-    checkInputs('signupForm', 'submitButton');
-  });
+document.getElementById('signupForm').addEventListener('input', function() {
+  checkInputs('signupForm', 'submitButton', 'index.html');
+});
 
-  document.getElementById('loginForm').addEventListener('input', function() {
-    checkInputs('loginForm', 'loginButton');
-  });
-
+document.getElementById('loginForm').addEventListener('input', function() {
+  checkInputs('loginForm', 'loginButton', 'index.html');
+});
